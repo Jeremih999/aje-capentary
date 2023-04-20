2bb25f6f178ca8673d119cd7e679819d9dbd2903
jeremih999 <brownjeremih@gmail.com>
2023-04-20 03:34:53 +0100

    message

--- src/component/Headers.js
+++ src/component/Headers.js
@@ -9,14 +9,14 @@ const Headers = ()=>{
             <button>Shop Now</button>
            </div>
            <div className="header-img__container">
-               <div className="img-1">
-                <img src="./img/one.jpg" alt="pic1"/>
+               <div>
+                <img src="./img/head1.jpg" alt="pic1"/>
                </div>
-               <div className="img-2">
-                <img src="./img/two.jpg" alt="pic2"/>
+               <div>
+                <img src="./img/head2.jpg" alt="pic2"/>
                </div>
-               <div className="img-3">
-                <img src="./img/three.jpg" alt="pic3"/>
+               <div>
+                <img src="./img/head3.jpg" alt="pic3"/>
                </div>
            </div>
         </header>
--- src/component/Testimonial.js
+++ src/component/Testimonial.js
@@ -1,10 +1,15 @@
 import React from "react";
 import testimonials from "../testimonials";
-// Import Swiper React components
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const Testimonial = () => {
 
@@ -14,10 +19,15 @@ const Testimonial = () => {
 
             <div className="testimonial__list">
                 <Swiper
-                spaceBetween={50}
-                slidesPerView={3}
-                onSlideChange={() => console.log('slide change')}
-                onSwiper={(swiper) => console.log(swiper)}
+                      // install Swiper modules
+                        modules={[Navigation, Pagination, Scrollbar, A11y]}
+                        spaceBetween={50}
+                        slidesPerView={1}
+                        navigation
+                        pagination={{ clickable: true }}
+                        // scrollbar={{ draggable: true }}
+                        onSwiper={(swiper) => console.log(swiper)}
+                        onSlideChange={() => console.log('slide change')}
                 >
                 {
                     testimonials.map((testimonial) => {
